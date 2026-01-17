2defb243b8da63bf6f3b90b51eab4bdda85108c2
Junkazama87 <64848168+loser-cmd@users.noreply.github.com>
2026-01-17 12:44:11 +0800

    Add dashboard index page with navigation links

---
+++ dashboard/index.php
@@ -0,0 +1,10 @@
+<?php include "../includes/auth_check.php"; ?>
+<h1>Dashboard Pimpinan Setda</h1>
+
+<ul>
+<li><a href="kinerja.php">Kinerja Bagian</a></li>
+<li><a href="anggaran.php">Realisasi Anggaran</a></li>
+<li><a href="kegiatan.php">Kegiatan Strategis</a></li>
+<li><a href="../auth/logout.php">Logout</a></li>
+</ul>
+
