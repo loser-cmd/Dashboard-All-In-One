e829d5a81cc774b067737f66da51e25d67b9b4bb
Junkazama87 <64848168+loser-cmd@users.noreply.github.com>
2026-01-17 18:01:43 +0800

    Delete auth /login.php

--- auth /login.php
+++
@@ -1,6 +0,0 @@
-<form method="post" action="process_login.php">
-<h2>Login Dashboard Pimpinan</h2>
-<input name="username" placeholder="Username">
-<input type="password" name="password" placeholder="Password">
-<button>Login</button>
-</form>
