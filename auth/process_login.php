e0ee59657272e9e5e0b02ef7dbd8286e7577484f
Junkazama87 <64848168+loser-cmd@users.noreply.github.com>
2026-01-19 10:21:01 +0800

    Update process_login.php

--- auth/process_login.php
+++ auth/process_login.php
@@ -3,15 +3,15 @@ session_start();
 include "../config/database.php";
 
 $u=$_POST['username'];
-$p=md5($_POST['password']);
+$p=$_POST['password'];
 
-$q=mysqli_query($conn,"SELECT * FROM users WHERE username='$u' AND password='$p'");
-$d=mysqli_fetch_array($q);
+$q=mysqli_query($conn,"SELECT * FROM users WHERE username='$u'");
+$d=mysqli_fetch_assoc($q);
 
-if($d){
- $_SESSION['login']=true;
- $_SESSION['role']=$d['role'];
- header("Location: ../dashboard/index.php");
-}else{
- echo "Login gagal";
+if($d && password_verify($p,$d['password'])){
+    $_SESSION['login']=true;
+    $_SESSION['role']=$d['role'];
+    header("Location:/modules/dashboard/index.php");
+    exit;
 }
+echo "Login gagal";
