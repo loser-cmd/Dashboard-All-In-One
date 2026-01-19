765b7bee6fcc2a18b8a5edb321fa2b277fd43025
Junkazama87 <64848168+loser-cmd@users.noreply.github.com>
2026-01-19 10:48:09 +0800

    Delete dashboard/kegiatan.php

--- dashboard/kegiatan.php
+++
@@ -1,8 +0,0 @@
-<?php include "../includes/auth_check.php";
-include "../config/database.php";
-
-$q=mysqli_query($conn,"SELECT * FROM kegiatan");
-while($d=mysqli_fetch_array($q)){
- echo $d['nama_kegiatan']." - ".$d['status']."<br>";
-}
-
