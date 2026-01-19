c8cacc92ddac754909fcee68a0144f1de10c0554
Junkazama87 <64848168+loser-cmd@users.noreply.github.com>
2026-01-19 10:47:39 +0800

    Delete dashboard/anggaran.php

--- dashboard/anggaran.php
+++
@@ -1,8 +0,0 @@
-<?php include "../includes/auth_check.php";
-include "../config/database.php";
-
-$q=mysqli_query($conn,"SELECT * FROM anggaran");
-while($d=mysqli_fetch_array($q)){
- echo $d['bagian']." - Rp".$d['realisasi']." / Rp".$d['pagu']."<br>";
-}
-
