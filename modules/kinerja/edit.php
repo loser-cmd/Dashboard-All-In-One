ae68288d69cfacf14fe8d7357396de0930e2840c
Junkazama87 <64848168+loser-cmd@users.noreply.github.com>
2026-01-19 11:06:20 +0800

    Create edit.php

---
+++ modules/kinerja/edit.php
@@ -0,0 +1,20 @@
+<?php
+include "../../includes/auth_check.php";
+include "../../includes/role_check.php";
+include "../../config/database.php";
+$id=$_GET['id'];
+$d=mysqli_fetch_assoc(mysqli_query($conn,"SELECT * FROM kinerja WHERE id=$id"));
+if($_POST){
+ mysqli_query($conn,"UPDATE kinerja SET
+ bagian='$_POST[bagian]',indikator='$_POST[indikator]',
+ capaian='$_POST[capaian]',target='$_POST[target]' WHERE id=$id");
+ header("Location:index.php");
+}
+?>
+<form method="post">
+<input name="bagian" value="<?=$d['bagian']?>">
+<input name="indikator" value="<?=$d['indikator']?>">
+<input name="capaian" value="<?=$d['capaian']?>">
+<input name="target" value="<?=$d['target']?>">
+<button>Update</button>
+</form>
