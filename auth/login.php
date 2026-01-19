3dfb07cd035a218b7712e772c1af3af632c6e5cb
Junkazama87 <64848168+loser-cmd@users.noreply.github.com>
2026-01-19 10:19:23 +0800

    Update login.php

--- auth/login.php
+++ auth/login.php
@@ -1,2 +1,5 @@
-<?php header("Location: auth/login.php"); ?>
-
+<form method="post" action="process_login.php">
+<input name="username" placeholder="Username" required>
+<input type="password" name="password" placeholder="Password" required>
+<button>Login</button>
+</form>
