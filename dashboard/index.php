497b85f08e3e4bd3cc06ffc8d7f9f8b419262db1
Junkazama87 <64848168+loser-cmd@users.noreply.github.com>
2026-01-19 10:09:53 +0800

    Update dashboard with additional links and info

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,10 +1,16 @@
-<?php include "../includes/auth_check.php"; ?>
-<h1>Dashboard Pimpinan Setda</h1>
+<?php
+include "../../includes/auth_check.php";
+include "../../config/fingerprint.php";
+?>
+<h2><?=APP_NAME?></h2>
+<p>Versi: <?=APP_VERSION?></p>
+<p>Unit: <?=APP_UNIT?></p>
+<p>Pemilik: <?=APP_OWNER?></p>
+<p>Fingerprint: <?=APP_FINGERPRINT?></p>
 
 <ul>
-<li><a href="kinerja.php">Kinerja Bagian</a></li>
-<li><a href="anggaran.php">Realisasi Anggaran</a></li>
-<li><a href="kegiatan.php">Kegiatan Strategis</a></li>
-<li><a href="../auth/logout.php">Logout</a></li>
+ <li><a href="../kinerja/index.php">Kinerja</a></li>
+ <li><a href="../anggaran/index.php">Anggaran</a></li>
+ <li><a href="../kegiatan/index.php">Kegiatan</a></li>
+ <li><a href="/auth/logout.php">Logout</a></li>
 </ul>
-
